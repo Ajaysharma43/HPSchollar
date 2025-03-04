d24405f8fceb0aa481ba46a42a607cd260de1943
ajay sharma <ajaysharma445446@gmail.com>
2025-03-04 19:36:11 +0530

    changes

--- app/Components/Homepage_Components/Cards_Component/page.jsx
+++ app/Components/Homepage_Components/Cards_Component/page.jsx
@@ -7,7 +7,7 @@ const Cards_Component = () => {
   return (
     <>
       <div className="flex flex-wrap justify-center gap-[30px] bg-[#FFFFFF] md:w-[1207.563232421875px] rounded-3xl pt-10 relative bottom-20 p-5">
-        <section className="bg-[#292929] w-auto md:w-[341.61566162109375px] h-[172.69000244140625px] rounded-[10px] text-center flex flex-col items-center justify-center">
+        <section className="bg-[#292929] transition-all duration-200 hover:bg-[#1a1919] w-auto md:w-[341.61566162109375px] h-[172.69000244140625px] rounded-[10px] text-center flex flex-col items-center justify-center">
           <Image
             src={Education_Icon}
             width={44.39681625366211}
@@ -24,7 +24,7 @@ const Cards_Component = () => {
             READ MORE <FaLongArrowAltRight className="mt-1" />
           </h1>
         </section>
-        <section className="bg-[#5AB943] w-auto md:w-[341.61566162109375px] h-[172.69000244140625px] rounded-[10px] text-center flex flex-col items-center justify-center">
+        <section className="bg-[#5AB943] transition-all duration-200 hover:bg-[#5b8e4e] w-auto md:w-[341.61566162109375px] h-[172.69000244140625px] rounded-[10px] text-center flex flex-col items-center justify-center">
           <Image
             src={Learn_Icon}
             width={44.39681625366211}
