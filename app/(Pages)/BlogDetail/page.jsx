bdbc8729c76e7a59c584677ef44ae51a5a9c2acf
ajay sharma <ajaysharma445446@gmail.com>
2025-03-11 13:07:36 +0530

    changes

--- app/(Pages)/BlogDetail/page.jsx
+++ app/(Pages)/BlogDetail/page.jsx
@@ -1,10 +1,12 @@
 import BlogDetailHeader from "@/app/Components/BlogDetailComponents/BlogDetail/BlogDetail";
 import BlogDetailBody2 from "@/app/Components/BlogDetailComponents/BlogDetailBody/BlogDetailBody";
 import BlogDetailBody from "@/app/Components/BlogDetailComponents/BlogDetalBody/BlogDetailBody";
+import { Suspense } from "react";
 
 const BlogDetail = () => {
     return(
         <>
+        <Suspense fallback={<div>loading</div>}>
         <header>
             <BlogDetailHeader/>
         </header>
@@ -13,6 +15,7 @@ const BlogDetail = () => {
             <section><BlogDetailBody/></section>
             <section><BlogDetailBody2/></section>
         </article>
+        </Suspense>
         </>
     )
 }
